e767e2458e3194973642475f07a416aff134773c
William Redenbaugh <51929010+william-redenbaugh@users.noreply.github.com>
2020-05-02 02:28:12 -0700

    Adds a general style for function comments to whole program
    So we can understand what each function in our program does!

--- udp_low.cpp
+++ udp_low.cpp
@@ -10,6 +10,12 @@ struct sockaddr_in client_address;
 // Socket ID Handlers
 int server_socket_fd = 0;
 
+/*
+Function: udp_server_socket_open(uint16_t port) 
+Description: Allows us to setup/host a udp server with a specified port. 
+Parameters: uint16_t port(port that we want to use for our server)
+Returns: none
+*/
 void udp_server_socket_open(uint16_t port){
     server_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(server_socket_fd < 0){
@@ -28,12 +34,24 @@ void udp_server_socket_open(uint16_t port){
     bind(server_socket_fd, (struct sockaddr*) &server_address, sizeof(server_address));
 }
 
+/*
+Function: udp_server_receive_blocking(uint8_t *array)
+Description: allows to check data we received from the UDP clients.  
+Parameters: uint8_t *array
+Returns: none
+*/
 uint16_t udp_server_receive_blocking(uint8_t *array){
     socklen_t len = 0;     
     uint16_t n = recvfrom(server_socket_fd, (char*)array, MAXLINE, MSG_WAITALL, (struct sockaddr*)&client_address, &len);
     return n;
 }
 
+/*
+Function: 
+Description: 
+Parameters: 
+Return: 
+*/
 void udp_server_send(uint16_t packet_size, uint8_t *array){
     sendto(server_socket_fd, array, packet_size, 0, (struct sockaddr*) &client_address, sizeof(client_address));
 }
