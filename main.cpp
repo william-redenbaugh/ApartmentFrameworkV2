a86a1df1a2322154167e0fbac1bee17eacc05609
William Redenbaugh <51929010+william-redenbaugh@users.noreply.github.com>
2020-05-04 22:57:15 -0700

    protobuffer testing added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 #include "MatrixControl.hpp"
 #include "StripAnimations.hpp"
 
+// Protobuffer stuff. 
+#include "status.pb.h"
+#include "pb.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+
 // Cap N Proto Libraries.  Unnecesarry for now, but useful later perhaps?
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
@@ -37,4 +43,21 @@ int main(void){
     const char *matrix_ip_addr = "192.168.1.39";
     matrix_control.begin(matrix_ip_addr, 4400, 64, 32);
     matrix_control.set_led(100, 100, 100, 1, 1);
+}
+
+void testing_protobuffers(void){
+    BoardStatus board_status; 
+    board_status.free_mem_kb = 500; 
+    board_status.status = 1; 
+
+    uint8_t buffer[128];
+    size_t msg_len; 
+
+    pb_ostream_t stream_out = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_encode(&stream_out, BoardStatus_fields, &board_status);
+
+    pb_istream_t stream_in  = pb_istream_from_buffer(buffer, msg_len);
+    
+    BoardStatus board_status_in; 
+    pb_decode(&stream_in, BoardStatus_fields, &board_status_in);
 }
