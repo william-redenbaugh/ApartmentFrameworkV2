ca48fad6f970af7324224996d1a6754c72934457
= <=>
2020-05-07 14:24:05 -0700

    added implementation for distinction between matrix and led strip data

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 // Protobuffer stuff. 
 #include "status.pb.h"
+#include "messagedata.pb.h"
 #include "pb.h"
 #include "pb_encode.h"
 #include "pb_decode.h"
@@ -15,7 +16,13 @@
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
 
+// Allows us to test and make sure protobuffers are working properly. 
+void testing_protobuffers(void);
+
 int main(void){
+    // Test to make sure protobufffers work properly. 
+    testing_protobuffers();
+
     // Remote control of strip testing. 
     UDPStripControl strip; 
     const char *strip_ip_addr = "192.168.1.2";
@@ -43,6 +50,8 @@ int main(void){
     const char *matrix_ip_addr = "192.168.1.39";
     matrix_control.begin(matrix_ip_addr, 4400, 64, 32);
     matrix_control.set_led(100, 100, 100, 1, 1);
+
+    return 0;
 }
 
 void testing_protobuffers(void){
@@ -60,4 +69,25 @@ void testing_protobuffers(void){
     
     BoardStatus board_status_in; 
     pb_decode(&stream_in, BoardStatus_fields, &board_status_in);
+}
+
+void testing_message_size_pb(void){
+    MessageData message_data_out;
+    message_data_out.message_size = 230;
+    message_data_out.message_type = MessageData_MessageType_MATRIX_DATA;
+
+    // Put data into serialized format. 
+    uint8_t buffer[32];
+    pb_ostream_t msg_out = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_encode(&msg_out, MessageData_fields, &message_data_out);
+
+    // Unpack serialsed data. 
+    pb_istream_t msg_in = pb_istream_from_buffer(buffer, 32);
+
+    MessageData message_data_in; 
+    pb_decode(&msg_in, MessageData_fields, &message_data_in);
+
+    if(message_data_in.message_size == message_data_out.message_size && message_data_in.message_type == message_data_out.message_type){
+        printf("Testing message data size succedded! \n");
+    }
 }
--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 CC=g++ -std=c++11 -pthread
 CFLAGS=-I.
-DEPS = udp_low.hpp UDPStripControl.hpp ClockControl.hpp HeartControl.hpp StripAnimations.hpp MatrixControl.hpp pb_common.h pb_decode.h pb_encode.h pb.h status.pb.h
+DEPS = udp_low.hpp UDPStripControl.hpp ClockControl.hpp HeartControl.hpp StripAnimations.hpp MatrixControl.hpp pb_common.h pb_decode.h pb_encode.h pb.h status.pb.h messagedata.pb.h
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-LEDControl:  main.o UDPStripControl.o  udp_low.o HeartControl.o ClockControl.o StripAnimations.o MatrixControl.o pb_common.o pb_decode.o pb_encode.o status.pb.o
-	$(CC) -o LEDControl main.o UDPStripControl.o udp_low.o HeartControl.o ClockControl.o StripAnimations.o MatrixControl.o pb_common.o pb_decode.o pb_encode.o status.pb.o
+LEDControl:  main.o UDPStripControl.o  udp_low.o HeartControl.o ClockControl.o StripAnimations.o MatrixControl.o pb_common.o pb_decode.o pb_encode.o status.pb.o messagedata.pb.o
+	$(CC) -o LEDControl main.o UDPStripControl.o udp_low.o HeartControl.o ClockControl.o StripAnimations.o MatrixControl.o pb_common.o pb_decode.o pb_encode.o status.pb.o messagedata.pb.o
+	
